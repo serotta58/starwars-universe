536b4d80d14fc2b7e16eb50b87cf2d3b7d564e18
GLEN CUNNINGHAM <glen@cunninghamconsulting.net>
2019-03-27 15:36:37 -0400

    Updates

---
+++ precache-manifest.48aa2a8cee5737df3173daa9de924f9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4350c5a0bca9f6d73942",
+    "url": "/starwars-universe/static/css/main.0d0852be.chunk.css"
+  },
+  {
+    "revision": "4350c5a0bca9f6d73942",
+    "url": "/starwars-universe/static/js/main.a01b9da4.chunk.js"
+  },
+  {
+    "revision": "156f2378597311ef2bbc",
+    "url": "/starwars-universe/static/js/runtime~main.954ab3bb.js"
+  },
+  {
+    "revision": "de5515665a2542d0f3e1",
+    "url": "/starwars-universe/static/js/2.3863f591.chunk.js"
+  },
+  {
+    "revision": "3df5869ba4957c6ac9ab2704d863bbda",
+    "url": "/starwars-universe/index.html"
+  }
+];
